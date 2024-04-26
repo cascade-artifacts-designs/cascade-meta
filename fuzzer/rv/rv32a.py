6f540d6fbf170556660e00457ff38da12ccc199a
=youzi27 <=809891392@qq.com>
2024-04-26 05:27:48 +0000

    Add rv32a extension

--- fuzzer/rv/rv32a.py
+++ fuzzer/rv/rv32a.py
@@ -1,6 +1,4 @@
-# Copyright 2023 Flavien Solt, ETH Zurich.
-# Licensed under the General Public License, Version 3.0, see LICENSE for details.
-# SPDX-License-Identifier: GPL-3.0-only
+# Copyright 202 Zihui Guo
 
 import rv.rvprotoinstrs as rvprotoinstrs
 import random
